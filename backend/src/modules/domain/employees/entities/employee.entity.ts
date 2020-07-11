030be1da0708b2e12c882873273d558490221850
Daniel Abrao <danielpaladar@gmail.com>
2020-07-11 03:10:33 -0300

    Try to fix syntax issue

--- backend/src/modules/domain/employees/entities/employee.entity.ts
+++ backend/src/modules/domain/employees/entities/employee.entity.ts
@@ -1,4 +1,4 @@
-import * as moment from 'moment-timezone';
+import moment from 'moment-timezone';
 import { AggregateRoot } from '../../../common/entities';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { CreateEmployee } from '../commands/create-employee.command';
@@ -37,7 +37,7 @@ export class Employee extends AggregateRoot<number> {
   }
 
   @PrimaryGeneratedColumn()
-  //change <string> to <number> in line 8 to fix the compile error
+  // change <string> to <number> in line 8 to fix the compile error
   id: number;
 
   @Column({ length: 100 })
