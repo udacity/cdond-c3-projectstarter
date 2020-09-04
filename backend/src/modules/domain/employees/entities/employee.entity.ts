48bad5603c6090bd74df77274e110a077b602d0d
Byron Sommardahl <byron@sommardahl.com>
2020-09-04 13:07:42 -0500

    Removing toDate.

--- backend/src/modules/domain/employees/entities/employee.entity.ts
+++ backend/src/modules/domain/employees/entities/employee.entity.ts
@@ -121,7 +121,7 @@ export enum SalaryType {
 }
 
 export function getDateFromString(date: string): Date {
-  return moment(date).format('M/D/YYYY').toDate();
+  return moment(date).format('M/D/YYYY');
 }
 
 function getGenderFromEnum(gender: string): Gender {
