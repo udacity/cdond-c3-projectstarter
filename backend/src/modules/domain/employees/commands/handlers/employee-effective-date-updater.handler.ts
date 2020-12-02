6c94f544df812f7fee187c24b52ef89409dc3c46
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2020-12-02 14:27:18 -0800

    Update employee-effective-date-updater.handler.ts

--- backend/src/modules/domain/employees/commands/handlers/employee-effective-date-updater.handler.ts
+++ backend/src/modules/domain/employees/commands/handlers/employee-effective-date-updater.handler.ts
@@ -1,4 +1,4 @@
-import * as moment from 'moment-timezone';
+const moment = require('moment');
 import { BaseCommandHandler } from '../../../../common/commands';
 import { UpdateEmployeeEffectiveDate } from '../update-employee-effective-date.command';
 import { CommandHandler } from '@nestjs/cqrs';
