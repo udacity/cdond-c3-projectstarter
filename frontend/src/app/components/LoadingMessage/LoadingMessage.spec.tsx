b83c8731e5c332e062e8395dde261d093f3b3d9b
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2020-12-04 14:38:09 -0800

    Update LoadingMessage.spec.tsx

--- frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
+++ frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
@@ -7,7 +7,7 @@ describe('<LoadingMessage>', () => {
     describe('message', () => {
       it('Should render the props message', () => {
         const message = 'Hello!';
-        const wrapper = shallow(<LoadingMessage message={message} />);
+        const wrapper = shallow(<LoadingMessage message={message} />)
         expect(wrapper.contains(<span>{message}?</span>)).toBeTruthy(); //remove the question mark to make the test pass
       });
     });
