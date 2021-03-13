a4d027800d7418949fa2b33586859692e08354a0
ShivakumarSwamy <shivakumarswamy.bg@gmail.com>
2021-03-13 16:06:53 +0100

    test phase: add missing oauth-sign module and cache in circle ci using package lock checksum

--- backend/src/modules/domain/employees/commands/handlers/employee-activator.handler.spec.ts
+++ backend/src/modules/domain/employees/commands/handlers/employee-activator.handler.spec.ts
@@ -19,7 +19,7 @@ describe('Employee Remover', () => {
       const handler = new EmployeeActivator(employeeRepository);
 
       const params = {
-        employeeId: 101, //change this to 100 to make the test pass
+        employeeId: 100,
         isActive: false,
       };
 
