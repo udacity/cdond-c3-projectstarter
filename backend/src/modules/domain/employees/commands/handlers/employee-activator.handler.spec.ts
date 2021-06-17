19d0c33a0449d130a74cf48755b27d881e39cece
Andre Noberto <andrenoberto@protonmail.com>
2021-06-17 22:33:09 +0100

    Fixed unit tests

--- backend/src/modules/domain/employees/commands/handlers/employee-activator.handler.spec.ts
+++ backend/src/modules/domain/employees/commands/handlers/employee-activator.handler.spec.ts
@@ -19,7 +19,7 @@ describe('Employee Remover', () => {
       const handler = new EmployeeActivator(employeeRepository);
 
       const params = {
-        employeeId: 101,
+        employeeId: 100,
         isActive: false,
       };
 
