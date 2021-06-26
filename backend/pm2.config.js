2fe8d721479fa180388dcb07317abc528a002abc
Andre Noberto <andrenoberto@protonmail.com>
2021-06-26 22:29:44 +0100

    Executing project in cluster mode

--- backend/pm2.config.js
+++ backend/pm2.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [{
     name      : 'UdaPeople',
     script    : '/home/ubuntu/backend/dist/main.js dotenv_config_path=/home/ubuntu/.env',
+    exec_mode : 'cluster_mode',
     node_args : '-r dotenv/config'
   }]
 };
