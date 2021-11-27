f1c021f69d516ccb6bb9378a77c58a882d145f86
Deepasj <94136422+Deepasj@users.noreply.github.com>
2021-11-27 20:16:31 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,7 +28,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth() x// here is an intentional compile error. Remove the "x" and the backend should compile.
+    .addBearerAuth() // here is an intentional compile error. Remove the "x" and the backend should compile.
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
