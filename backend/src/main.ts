211e4b208ccaf8fe3c090732b14d4fd629ad8da3
Azorji Kelechi Oliver <kelechioliver96@gmail.com>
2022-01-10 06:12:35 +0100

    fix build error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -28,8 +28,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth()x // here is an intentional compile error. Remove the "x" and the backend should compile.
+    .addBearerAuth()
     .build();
+    
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
   const config: ConfigService = app.get('ConfigService');
