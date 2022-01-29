1a9c0ac1bc35334643fca4b0b9d6a67206f662ad
Ovidiu Anicai <ovidiu.anicai@emag.ro>
2022-01-29 18:05:07 +0200

    project 04. fix rds db access

--- .circleci/create-db.sh
+++ .circleci/create-db.sh
@@ -2,6 +2,12 @@
 
 export db_name="udapeople-$RUN_ID"
 
+export VPC_ID=$(aws ec2 describe-vpcs | jq .Vpcs[0].VpcId -r)
+
+export SG_ID=$(aws ec2 create-security-group --group-name db-postgress-open --description "db postgress open" --vpc-id $VPC_ID | jq .GroupId -r)
+
+aws ec2 authorize-security-group-ingress --group-id $SG_ID --protocol tcp --port 5432 --cidr 0.0.0.0/0
+
 aws rds create-db-instance \
     --db-instance-identifier ${db_name} \
     --db-instance-class db.t3.micro \
@@ -10,5 +16,6 @@ aws rds create-db-instance \
     --master-user-password czateny7r9zcwkaubdp5j \
     --port 5432 \
     --publicly-accessible \
+    --vpc-security-group-ids $SG_ID \
     --no-multi-az \
     --allocated-storage 20
