a3c600d024a2d524a1864cd4df63031e997ca387
Ovidiu Anicai <ovidiu.anicai@emag.ro>
2022-01-29 19:48:38 +0200

    project 04. fix ansible unarchive issue

--- .circleci/create-db.sh
+++ .circleci/create-db.sh
@@ -2,7 +2,10 @@
 
 export db_name="udapeople-$RUN_ID"
 
+# https://docs.aws.amazon.com/cli/latest/reference/ec2/describe-vpcs.html
 export VPC_ID=$(aws ec2 describe-vpcs | jq .Vpcs[0].VpcId -r)
+
+# https://docs.aws.amazon.com/cli/latest/userguide/cli-services-ec2-sg.html
 
 if `aws ec2 create-security-group --group-name db-postgress-open --description "db postgress open" --vpc-id $VPC_ID` ; then
     echo "waiting to create Security Group"
@@ -14,6 +17,7 @@ else
     export SG_ID=$(aws ec2 describe-security-groups --group-name db-postgress-open | jq .SecurityGroups[0].GroupId -r)
 fi
 
+# https://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
 aws rds create-db-instance \
     --db-name postgress \
     --db-instance-identifier ${db_name} \
