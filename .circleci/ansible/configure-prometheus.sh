6c3ca5689f8c1b9634c3652772e5e9d4969f2f08
Ovidiu Anicai <ovidiu.anicai@emag.ro>
2022-02-12 15:13:24 +0200

    fix cleanup, modified prometheus setup

---
+++ .circleci/ansible/configure-prometheus.sh
@@ -0,0 +1,2 @@
+
+ansible-playbook -i inventory-secondary.txt configure-prometheus.yml
--- .circleci/manual/create-cloudfront.sh
+++ .circleci/manual/create-cloudfront.sh
@@ -4,3 +4,4 @@ aws cloudformation deploy \
     --template-file ../files/cloudfront.yml \
     --stack-name InitialStack\
     --parameter-overrides WorkflowID=$RUN_ID
+
