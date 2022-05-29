e147419d3af4ede9a4f75ceb3aea4eda441c6adc
FerSherlockBclear <aguilar.b.fermin@gmail.com>
2022-05-29 09:02:23 +0100

    fixed

--- frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
+++ frontend/src/app/components/LoadingMessage/LoadingMessage.spec.tsx
@@ -8,7 +8,7 @@ describe('<LoadingMessage>', () => {
       it('Should render the props message', () => {
         const message = 'Hello!';
         const wrapper = shallow(<LoadingMessage message={message} />);
-        expect(wrapper.contains(<span>{message}</span>)).toBeTruthy(); //remove the question mark to make the test pass
+        expect(wrapper.contains(<span>{message}?</span>)).toBeTruthy(); //remove the question mark to make the test pass
       });
     });
   });
